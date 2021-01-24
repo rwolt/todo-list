1ec1b4186bd31d6d404909d9e8ce2541d85a3a4f
Raymond Wolt <rjwolt@gmail.com>
2021-01-23 18:46:03 -0600

    Selecting project refreshes Task List Panel

--- src/index.js
+++ src/index.js
@@ -40,20 +40,82 @@ Task.prototype.markImportant = function() {
     this.important = true;
 };
 
+Task.prototype.markNotImportant = function() {
+    this.important = false;
+};
+
+//Module for controlling DOM elements
 let displayController = (function() {
-  
+
 let updateProjects = function() {
         let listEl = document.createElement('p');
         listEl.innerText = Projects[Projects.length - 1].name;
-        document.getElementById('projects-list').appendChild(listEl);
-    }; 
+        listEl.dataset.index = `${Projects.length - 1}`
+        let projectsList = document.getElementById('projects-list')
+        projectsList.appendChild(listEl);
+        listEl.addEventListener('click', (e) => {
+            document.querySelector('#list-panel').appendChild(
+                displayController.createList(Projects[e.target.dataset.index])); 
+            if(document.querySelector('#selected')) {
+                document.querySelector('#selected').removeAttribute('id');
+                e.target.id = 'selected';
+            } else {
+                e.target.id = 'selected';
+            }
+        });
+} 
 
+let createList = function(project) {
+        //Create Title Element
+        let projTitle = document.createElement('p');
+        projTitle.className = 'title';
+        projTitle.innerText = project.name;
+       //Create Todo List Element
+        let todoList = document.createElement('div');
+        todoList.className = 'todo-list';
+        //Create Form and Form Elements for New Todo
+        let newTodoForm = document.createElement('form');
+        newTodoForm.id = 'new-todo-form';
+        let addTodoBtn = document.createElement('button');
+        addTodoBtn.innerText = "+";
+        addTodoBtn.id = 'add-todo-btn';
+        let newTodoName = document.createElement('input');
+        newTodoName.id = 'new-todo-name';
+        addTodoBtn.addEventListener('click', addTodoHandler);
+        //Append Form Elements to Form
+        newTodoForm.appendChild(addTodoBtn);
+        newTodoForm.appendChild(newTodoName);
+        //Append All Elements to List
+        todoList.appendChild(projTitle);
+        todoList.appendChild(newTodoForm);
+        todoList.appendChild(updateTodos(project));
 
-    return {updateProjects};
+        return todoList;
+}
+
+let updateTodos = function(project) {
+    //Populate Todo List Element
+    let list = document.createElement('div');
+    list.id = 'task-list';
+    for (let todo of project.todos) {
+        let item = document.createElement('p');
+        item.innerText = todo.name;
+        list.appendChild(item);
+    }
+    return list;
+}
+
+let clearList = function() {
+    let list = document.querySelector('#task-list');
+    list.parentElement.removeChild(list);
+}
+
+    return { updateProjects, createList, clearList, updateTodos };
 })();
 
 let projName = document.querySelector('#new-project-name');
 let addProj = document.querySelector('#add-project-btn');
+let addTodo = document.querySelector('#add-todo-btn');
 
 addProj.addEventListener('click', (e) => {
     e.preventDefault();
@@ -61,5 +123,16 @@ addProj.addEventListener('click', (e) => {
     Projects.push(newProj);
     displayController.updateProjects();
     document.querySelector('#new-project-form').reset();
-})
+});
 
+let addTodoHandler = (e) => {
+    e.preventDefault();
+    let currentIndex = document.querySelector('#selected').dataset.index;
+    let todoName = document.querySelector('#new-todo-name').value;
+    let currentProject = Projects[currentIndex];
+    currentProject.addTask(todoName);
+    displayController.clearList();
+    document.querySelector('.todo-list').appendChild(displayController.updateTodos(currentProject));
+    document.querySelector('#new-todo-form').reset();
+   
+};
